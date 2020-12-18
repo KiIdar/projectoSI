a5653deacf470ced551933a3e70209be44d50d0a
Gonçalo <g.junqueira@ua.pt>
2020-12-18 18:00:37 +0000

    - Calculadora como programa principal - Biblioteca criada e iniciada - Codigo para a info do PC mudada para a biblioteca - PEQUENA mudança nos prints

--- projectoSI/src/Testes/ProjectoSI.java
+++ projectoSI/src/Testes/ProjectoSI.java
@@ -1,9 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Testes;
+
+import Protecao.Protecao;
+import InformacaoSistema.CPU;
+import InformacaoSistema.HostName;
+import InformacaoSistema.MAC;
+import InformacaoSistema.Motherboard;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +17,48 @@ public class ProjectoSI {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Ola Rafael!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        /*Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+
+        // nextDouble() reads the next double from the keyboard
+        double first = reader.nextDouble();
+        double second = reader.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = reader.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = first + second;
+                break;
+
+            case '-':
+                result = first - second;
+                break;
+
+            case '*':
+                result = first * second;
+                break;
+
+            case '/':
+                result = first / second;
+                break;
+
+            // operator doesn't match any case constant (+, -, *, /)
+            default:
+                System.out.printf("Error! operator is not correct");
+                return;
+        }
+
+        System.out.println(first + " " + operator + " " + second + " = " + result);*/
+
+        //Biblioteca adicionada e testtada
+        Protecao test = new Protecao();
+        test.getPCInfo();
+
     }
-    
 }
