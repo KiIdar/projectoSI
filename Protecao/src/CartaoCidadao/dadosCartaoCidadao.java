62a57d69271eb278f1088ba1a2b21f50ea078985
rafaelff97 <rFael_f_fernandes@hotmail.com>
2020-12-27 16:27:12 +0000

    .

--- Protecao/src/CartaoCidadao/dadosCartaoCidadao.java
+++ Protecao/src/CartaoCidadao/dadosCartaoCidadao.java
@@ -5,8 +5,6 @@ package CartaoCidadao;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import java.security.KeyStore;
 import java.security.Provider;
 import java.security.Security;
@@ -18,9 +16,9 @@ import java.util.Enumeration;
  * @author ASUS
  */
 public class dadosCartaoCidadao {
-       //Mostra o nome da pessoa do cc
+    //Mostra o nome da pessoa do cc
 
-    public void providers(){
+    public void providers() {
         Provider[] prov = Security.getProviders();
 
         for (int i = 0; i < prov.length; i++) {
@@ -31,8 +29,8 @@ public class dadosCartaoCidadao {
     public Certificate provider() throws Exception {
         Provider prov = Security.getProvider("SunPKCS11-CartaoCidadao");
         KeyStore ks = null;
-        ks = KeyStore.getInstance("PKCS11",prov);
-        ks.load(null,null);
+        ks = KeyStore.getInstance("PKCS11", prov);
+        ks.load(null, null);
         Enumeration<String> als = ks.aliases();
         Certificate c = ks.getCertificate("CITIZEN AUTHENTICATION CERTIFICATE");
         //System.out.println("Certificado: " + c);
@@ -45,12 +43,13 @@ public class dadosCartaoCidadao {
         //System.out.println(tudoCertificado);
         String[] testeNome = tudoCertificado.split("CN=");
         String nome[] = testeNome[1].split(",");
-        System.out.println("Nome:"+nome[0]);
+        System.out.println("Nome:" + nome[0]);
 
         return null;
     }
-    
-     public static void main(String[] args) throws Exception {
+
+    //Teste
+    public static void main(String[] args) throws Exception {
         dadosCartaoCidadao d = new dadosCartaoCidadao();
         d.providers();
         d.provider();
